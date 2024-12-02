f26686488deca4105d7fcd02bef6629c37b34e27
LaszloF5 <fazekaslaszlo31@gmail.com>
2024-12-02 20:29:34 +0100

    Modify header.

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import CreateCard from './Components/CreateCard';
 
@@ -6,7 +5,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Memory card</h1>
       </header>
       <CreateCard />
